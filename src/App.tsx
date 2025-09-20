5e39545525c6ffab3013127da41f4ef7a7907a24
33622 <177898417+33622@ users.noreply.github.com>
2025-09-20 16:55:08 +0900

    デフォルトの内容から更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { User } from './types'
+import UserList from './UserList';
+import UserForm from './UserForm';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([]);
+
+  const addUser = (name: string) => {
+    const newUser = { id: Date.now(), name }
+    setUsers([...users, newUser]);
+};
+
+  const deleteUser = (id: number) => {
+    setUsers(users.filter(user => user.id !== id));
+
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>ユーザー一覧</h1>
+      <UserForm onAddUser={addUser} />
+      <UserList users={users} onDeleteUser={deleteUser} />
+    </div>
   )
 }
 
-export default App
+export default App;
